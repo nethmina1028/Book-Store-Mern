1464bb9325c13711eedef0b40635c87d56c61643
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-09 22:26:18 +0530

    Routers setup and react dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
-import './App.css'
+import React from 'react'
+import { Routes ,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 function App() {
  
   return (
-    <>
-     <h1 className='bg-red-600'>d</h1>
-    </>
+   <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+   </Routes>
   )
 }
 
