216b30075198b903aead3f5d84e5a7bf1101ffd1
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-09 10:51:21 +0530

    database and server done

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
-export const PORT = 5555
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://nethmina1028:pabasara@bookapp.4vrvshf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookApp'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,33 @@
-import express from 'express';
-import {PORT} from './config.js';
+import express, { request } from 'express'; //1
+import {PORT,mongoDBURL} from './config.js'; //3
+import mongoose from 'mongoose';  //5
 
-const app = express();
+const app = express(); //2
 
-app.listen(PORT,()=>{
+app.get('/',(request,response)=>{   //4
+   // console.log(request)
+    return response.status(234).send('Hello World')
+});
+
+
+/*  go this code true in to mongoose conntion
+app.listen(PORT,()=>{          //3
     console.log(`Server is running on port ${PORT}`);
-})
+});
+
+*/
+
+
+mongoose                      //5
+    .connect(mongoDBURL)
+
+    .then(()=>{
+        console.log('MongoDB connected');
+        app.listen(PORT,()=>{          //3
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    })
+    .catch((error)=>{
+         console.log(error);
+    });
