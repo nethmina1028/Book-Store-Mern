5f77cd1f55ce3d723e3fcd0301167da45f2cf47d
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-10 15:00:45 +0530

    complite Add Show pages

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
+import React,{useState} from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-function CreateBook() {
+const CreateBook =() =>{
+  const [title,setTitle] = useState('');
+  const [author,setAuthor] = useState('');
+  const [publishYear,setPublishYear] = useState('');
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () =>{
+    const data = {
+    title,
+    author,
+    publishYear,
+   };
+
+    setLoading(true);
+
+    axios
+       .post('http://localhost:5555/books',data)
+       .then(() =>{
+        setLoading(false);
+        navigate('/');
+       })
+       .catch((error) =>{
+        setLoading(false);
+        alert('An error happend.Plz cheak console');
+        console.log(error);
+       });
+      };
   return (
-    <div>CreateBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='my-4 text-3xl'>Create Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex-col border-2 rounded-xl w-[600px] border-sky-400 p-4 mx-auto'>
+
+        <div className='my-4'>
+              <label className='mr-4 text-xl text-gray-500 '>Title</label>
+
+              <input
+              type='text'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className='w-full px-4 py-2 border-2 border-gray-500'
+               />
+        </div>
+
+        <div className='my-4'>
+              <label className='mr-4 text-xl text-gray-500 '>Author</label>
+
+              <input
+              type='text'
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className='w-full px-4 py-2 border-2 border-gray-500'
+               />
+        </div>
+
+        <div className='my-4'>
+              <label className='mr-4 text-xl text-gray-500 '>Publish yea</label>
+
+              <input
+              type='text'
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className='w-full px-4 py-2 border-2 border-gray-500'
+               />
+        </div>
+
+        <button className='p-2 m-8 bg-sky-300' onClick={handleSaveBook}>
+            Save
+        </button>
+
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,11 +25,11 @@ function Home() {
     },[]);
   return (
     <div className='p-4'>
-       <div className="flex justify-between items-center">
-        <h1 className='text-3xl my-8'>Book List</h1>
+       <div className="flex items-center justify-between">
+        <h1 className='my-8 text-3xl'>Book List</h1>
 
         <Link to='/books/create'>
-        <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        <MdOutlineAddBox className='text-4xl text-sky-800' />
         </Link>
        </div>
        {loading ? (
@@ -38,35 +38,35 @@ function Home() {
           <table className='w-full border-separate border-spacing-2'>
             <thead>
                <tr>
-                <th className='border border-slate-600 rounded-md'>No</th>
-                <th className='border border-slate-600 rounded-md'>Title</th>
-                <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-                <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
-                <th className='border border-slate-600 rounded-md'>Operations</th>
+                <th className='border rounded-md border-slate-600'>No</th>
+                <th className='border rounded-md border-slate-600'>Title</th>
+                <th className='border rounded-md border-slate-600 max-md:hidden'>Author</th>
+                <th className='border rounded-md border-slate-600 max-md:hidden'>Publish Year</th>
+                <th className='border rounded-md border-slate-600'>Operations</th>
                </tr>
             </thead>
             <tbody>
                {books.map((book,index) => (
                 <tr key={book._id} className='h-8'>
 
-                <td className='border border-slate-700 rounded-md text-center'>
+                <td className='text-center border rounded-md border-slate-700'>
                     {index + 1}
                 </td>
 
-                <td className='border border-slate-700 rounded-md text-center'>
+                <td className='text-center border rounded-md border-slate-700'>
                     {book.title}
                 </td>
 
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                <td className='text-center border rounded-md border-slate-700 max-md:hidden'>
                     {book.author}
                 </td>
 
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                <td className='text-center border rounded-md border-slate-700 max-md:hidden'>
                     {book.publishYear}
                 </td>
                  
                  
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                <td className='text-center border rounded-md border-slate-700 max-md:hidden'>
                 <div className='flex justify-center gap-x-4'>
 
                     <Link to={`/books/details/${book._id}`}>
